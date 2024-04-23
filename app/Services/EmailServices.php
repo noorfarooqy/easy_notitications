19eefe516235a791cb1d2e0094f39ff1af20d42f
noorfarooqy <noor@drongo.vip>
2024-04-23 15:33:48 +0300

    update the email body

--- app/Services/EmailServices.php
+++ app/Services/EmailServices.php
@@ -30,10 +30,16 @@ class EmailServices extends NoorServices
     public function SendEmail($data,$subject='Easy Email', $view_template='en::mail.easy_notification_template')
     {
         try {
-            $email = EasyEmail::create($data);
+            $email = EasyEmail::create([
+                'to' => $data['to'],
+                'email_body' => is_array($data['email_body']) ? json_encode($data['email_body']) : $data['email_body'],
+                'sent_by' => auth()?->id(),
+            ]);
             Mail::to($data['to'])->send(new EasyNotificationMail($data['email_body'], $subject, $view_template));
             $this->setError('',0);
             $this->setSuccess('success');
+            $email->is_sent = true;
+            $email->save();
             return $this->getResponse($email);
         } catch (\Throwable $th) {
             $this->setError(env('APP_DEBUG')? $th->getMessage() : 'Error sending the email');
